f1be616bf479ffdfebb20e9ebb4f0d05fa0ed23e
minutestatistique <data.science.fr@gmail.com>
2017-11-09 18:59:36 +0100

    parallelized offers scraping

--- src/R/lbc.R
+++ src/R/lbc.R
@@ -12,11 +12,28 @@ cat_data <- html_text(cat_data_html)
 cat_data <- str_replace_all(cat_data, "\ ", "")
 cat_data <- as.numeric(cat_data)
 
-offres_data_html <- html_nodes(webpage, ".item_title")
-offres_data <- html_text(offres_data_html)
-
 expect_equal(cat_data[1], cat_data[2] + cat_data[3])
 nb_pages <- ceiling(cat_data[3] / length(offres_data))
 
-begin <- "https://www.leboncoin.fr/ventes_immobilieres/offres/ile_de_france/occasions/?o=1&f=c"
-end <- "https://www.leboncoin.fr/ventes_immobilieres/offres/ile_de_france/occasions/?o=23863&f=c"
+nb <- numeric(23944)
+
+# require(doParallel)
+
+# cl <- makeCluster(8)
+# registerDoParallel(cl)
+
+# nb <- foreach(r = 1:23944,
+              # .packages = c("rvest")#,
+            # .export = "dtU"
+              # ) %dopar% {
+for (r in 1:23944) {
+  url <- paste0("https://www.leboncoin.fr/ventes_immobilieres/offres/ile_de_france/occasions/?o=", r, "&f=c")
+  webpage <- read_html(url)
+  
+  offres_data_html <- html_nodes(webpage, ".item_title")
+  offres_data <- html_text(offres_data_html)
+  
+  nb[r] <- length(offres_data)
+}
+
+# stopCluster(cl)
